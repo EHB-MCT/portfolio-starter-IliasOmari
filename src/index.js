062ed5750fbfbc3bae0c6a0c01c7cc782f4bab42
IliasOmari <ilias.omari@student.ehb.be>
2023-10-12 11:16:53 +0200

    feat: adding postMessage endpoint

--- src/index.js
+++ src/index.js
@@ -12,6 +12,20 @@ app.get("/messages", async (req, res) => {
   const messages = await knex("messages").select();
   res.json(messages);
 });
+
+app.post("/message", async (req, res) => {
+  if (!req.body.content) {
+    return res.status(400).send({ error: "Fill the missings fields" });
+  }
+
+  const newMessage = {
+    content: req.body.content,
+  };
+
+  await knex("messages").insert(newMessage);
+  res.status(200).send("Message posted successfully");
+});
+
 app.listen(port, (err) => {
   if (!err) {
     console.log("running on port " + port);
