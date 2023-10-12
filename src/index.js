8a396792c600967d11ad801b3c0a2dd962ad357a
IliasOmari <ilias.omari@student.ehb.be>
2023-10-12 11:25:29 +0200

    feat: adding getMessage by Id endpoint

--- src/index.js
+++ src/index.js
@@ -13,6 +13,12 @@ app.get("/messages", async (req, res) => {
   res.json(messages);
 });
 
+app.get("/messages/:id", async (req, res) => {
+  const messageId = req.params.id;
+  const messages = await knex("messages").where("id", messageId);
+  res.json(messages);
+});
+
 app.post("/message", async (req, res) => {
   if (!req.body.content) {
     return res.status(400).send({ error: "Fill the missings fields" });
