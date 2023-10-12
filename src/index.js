e553541c89b4ce3e11a2d4bfe1f0426c535c27e8
IliasOmari <ilias.omari@student.ehb.be>
2023-10-12 11:09:18 +0200

    feat: adding getMessage endpoint

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
+app.get("/messages", async (req, res) => {
+  const messages = await knex("messages").select();
+  res.json(messages);
+});
 app.listen(port, (err) => {
   if (!err) {
     console.log("running on port " + port);
