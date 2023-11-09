95559af730c5d5f5f95424525ed9a79854fa78c7
IliasOmari <ilias.omari@student.ehb.be>
2023-11-09 13:00:22 +0100

    connected node project with postgres db

--- images/api/src/index.js
+++ images/api/src/index.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 const knex = require("./database.js");
 require("dotenv").config();
+
 const port = 3000;
 app.use(express.json());
 app.use(cors());
