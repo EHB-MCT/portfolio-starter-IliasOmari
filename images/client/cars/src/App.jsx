8ec19d029cf03d30a78c8764edafd20ebf620067
IliasOmari <ilias.omari@student.ehb.be>
2023-12-13 14:03:50 +0100

    Installed Framer Motion and applied on div's

--- images/client/cars/src/App.jsx
+++ images/client/cars/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { motion } from "framer-motion";
 
 function App() {
   const [cars, setCars] = useState([]);
@@ -33,7 +34,16 @@ function App() {
       <div className="models">
         {cars.map((cars, index) => {
           return (
-            <div key={index} className="model">
+            <motion.div
+              key={index}
+              className="model"
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.8,
+                ease: [0, 0.71, 0.2, 1.01],
+              }}
+            >
               <div className="projectImg">
                 <img src={cars.img} alt="car" />
               </div>
@@ -45,12 +55,17 @@ function App() {
                 </div>
 
                 <div className="button">
-                  <button onClick={() => handleDelete(cars.carModel)}>
-                    Delete
-                  </button>
+                  <motion.button
+                    onClick={() => handleDelete(cars.carModel)}
+                    whileHover={{ scale: 1.2 }}
+                    whileTap={{ scale: 0.9 }}
+                    transition={{ type: "spring", stiffness: 400, damping: 17 }}
+                  >
+                    Delete car
+                  </motion.button>
                 </div>
               </div>
-            </div>
+            </motion.div>
           );
         })}
       </div>
